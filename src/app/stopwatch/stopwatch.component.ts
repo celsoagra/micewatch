43f5e14bc8c5e0c6ed08c3330a5ac9cc46e3f187
Celso Agra <celsoagra@gmail.com>
2018-09-22 00:28:45 -0300

    init app micewatch

---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stopwatch',
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.css']
+})
+export class StopwatchComponent implements OnInit {
+
+
+  totalElapsedMs = 0;
+  elapsedMs = 0;
+  startTime;
+  timerPromise;
+  timesTurn = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    
+    
+    getTurns = function() {
+      return this.timesTurn;
+    }
+    
+    start = function() {
+      if (!this.timerPromise) {
+        this.startTime = new Date();
+        this.timerPromise = setInterval(() => {
+          var now = new Date();
+          this.elapsedMs = now.getTime() - this.startTime.getTime();
+        }, 31);
+      }
+    };
+    
+    stop = function() {
+      if (this.timerPromise) {
+        clearInterval(this.timerPromise);
+        this.timerPromise = undefined;
+        this.totalElapsedMs += this.elapsedMs;
+        this.elapsedMs = 0;
+      }
+    };
+    
+    reset = function() {
+      this.startTime = new Date();
+      this.totalElapsedMs = this.elapsedMs = 0;
+    };
+    
+    turn = function() {
+      this.timesTurn.push(this.getElapsedMsFormated());
+      this.reset();
+    };
+    
+    getTime = function() {
+      return this.time;
+    };
+    
+    getElapsedMs = function() {
+      return this.totalElapsedMs + this.elapsedMs;
+    };
+    
+    getElapsedMsFormated = function() {
+
+      var milliseconds = this.totalElapsedMs + this.elapsedMs;
+      
+      var hours = Math.floor(milliseconds / 3600000);
+      var minutes = Math.floor((milliseconds - (hours * 3600000)) / 60000);
+      var seconds = Math.floor((milliseconds - (hours * 3600000) - (minutes * 60000)) / 1000);
+      var milisec = Math.floor((milliseconds - (hours * 3600000) - (minutes * 60000) - seconds * 1000));
+
+      milisec = this.completeWithZeros(milisec+"", 3);
+      seconds = this.completeWithZeros(seconds+"", 2);
+      minutes = this.completeWithZeros(minutes+"", 2);
+      hours = this.completeWithZeros(hours+"", 2);
+      
+      return hours + ":" + minutes + ":" + seconds + " '" + milisec;
+  };
+
+  completeWithZeros = function(numberToZero:string, size:number) {
+    let timeWithZero = numberToZero;
+    while (timeWithZero.length < size) timeWithZero = "0" + timeWithZero;
+    return timeWithZero;
+  };
+
+}
